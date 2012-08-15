fbf8364a6d0ebb29b0d24fe0233f7fd9dca6a25b
John Heintz <john@gistlabs.com>
2012-08-14 20:40:39 -0500

    Add parameterized Cluster test

---
+++ src/test/java/com/gistlabs/bigdata/littletests/hadoop/wordcount/integration/MultipleClusterTest.java
@@ -0,0 +1,35 @@
+package com.gistlabs.bigdata.littletests.hadoop.wordcount.integration;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+@RunWith(value = Parameterized.class)
+public class MultipleClusterTest extends HadoopClusterBase {
+
+	public MultipleClusterTest(int ignore) {
+	}
+
+	@Parameters
+	public static Collection<Object[]> data() {
+		int size = 50;
+		Collection<Object[]> results = new ArrayList<Object[]>(size);
+		for (int i = 1; i < size; i++) { // zero gets messed up, test separately
+			results.add(new Object[] { i });
+		}
+		return results;
+	}
+
+	/**
+	 * Just test the machinery works... startup and shutdown.
+	 */
+	@Test
+	public void smokeTest() {
+		System.out.println("success!");
+	}
+}
